4b341c5264ecede0ecaf706336abc561675313a4
지혁아빠 <chaekb@jihyeog-appaui-Macmini.local>
2026-06-13 14:59:05 +0900

    feat: move answer choices into practice area

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yeonseo-math-v9";
+const CACHE_NAME = "yeonseo-math-v10";
 const APP_SHELL = [
   new URL("./", self.location).toString(),
   new URL("./index.html", self.location).toString(),
