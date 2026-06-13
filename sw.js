504c9d73cacb40090e437348583d58932a63e1cc
지혁아빠 <chaekb@jihyeog-appaui-Macmini.local>
2026-06-13 14:05:09 +0900

    feat: add daily missions and arithmetic modes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yeonseo-math-v7";
+const CACHE_NAME = "yeonseo-math-v8";
 const APP_SHELL = [
   new URL("./", self.location).toString(),
   new URL("./index.html", self.location).toString(),
