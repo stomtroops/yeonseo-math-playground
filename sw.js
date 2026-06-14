dd7ed795716f9b475feb732708a2e6461592ffc8
지혁아빠 <chaekb@jihyeog-appaui-Macmini.local>
2026-06-14 11:15:55 +0900

    feat: add counting match and bundle practice

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yeonseo-math-v16";
+const CACHE_NAME = "yeonseo-math-v17";
 const APP_SHELL = [
   new URL("./", self.location).toString(),
   new URL("./index.html", self.location).toString(),
