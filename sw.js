4cb1ff8367ecf554da6eda58d9ddf2cdeea04751
지혁아빠 <chaekb@jihyeog-appaui-Macmini.local>
2026-06-14 12:09:17 +0900

    feat: hide settings behind difficulty tab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yeonseo-math-v19";
+const CACHE_NAME = "yeonseo-math-v20";
 const APP_SHELL = [
   new URL("./", self.location).toString(),
   new URL("./index.html", self.location).toString(),
