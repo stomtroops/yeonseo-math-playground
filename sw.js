c3d3ac50c3de3bcae6c810aa8fd4ab2116edbcc3
지혁아빠 <chaekb@jihyeog-appaui-Macmini.local>
2026-06-14 17:43:12 +0900

    fix: keep count dots ungrouped

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yeonseo-math-v21";
+const CACHE_NAME = "yeonseo-math-v22";
 const APP_SHELL = [
   new URL("./", self.location).toString(),
   new URL("./index.html", self.location).toString(),
